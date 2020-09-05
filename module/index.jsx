bb3ac055130b69d72e17ed58e0844338d1b2db1a
vannoel <vannoel0628@gmail.com>
2020-09-05 15:45:19 +0800

    remove unnecessary

--- module/index.jsx
+++ module/index.jsx
@@ -2,11 +2,6 @@ import './styles/index.less';
 import Button from './components/Button.jsx';
 import ButtonGroup from './components/ButtonGroup.jsx';
 
-export default {
-  Button,
-  ButtonGroup
-};
-
 export {
   Button,
   ButtonGroup
